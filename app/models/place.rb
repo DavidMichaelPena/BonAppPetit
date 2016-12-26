e7bdcedd39bbb7538dc93b254e45a0b6c6c30c5f
DavidMichaelPena <alexamenos@gmail.com>
2016-12-26 18:44:49 +0000

    additional validations

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,7 @@
 class Place < ActiveRecord::Base
   belongs_to :user
   validates :name, presence: true
+  validates :name, length: { minimum: 3 }
+  validates :address, length: { minimum: 8}
+  validates :description, length: {in: 5..50}
 end
