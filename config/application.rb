4301d3fd7541a60330de4e6517f2aad5977a5c42
DavidMichaelPena <alexamenos@gmail.com>
2017-01-08 23:57:32 +0000

    Adjust production.rb so it compiles assets

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
-module Nomster
+module BonAppPetit
   class Application < Rails::Application
 
     
