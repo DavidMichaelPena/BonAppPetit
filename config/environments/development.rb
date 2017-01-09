3ab237a45be21a7e4a386e812c1294f02641d624
DavidMichaelPena <alexamenos@gmail.com>
2017-01-09 06:09:49 +0000

    fixing the fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
     :enable_starttls_auto => true
     }
 
-  #config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
 
   #config.action_mailer.smtp_settings = {
    # address: "smtp.gmail.com",
