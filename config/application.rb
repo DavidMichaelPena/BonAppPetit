b554f7edff9f66222a14b97b47ed8730d52132b9
DavidMichaelPena <alexamenos@gmail.com>
2017-01-09 00:33:55 +0000

    attempting to fix repository

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
-module BonAppPetit
+module Nomster
   class Application < Rails::Application
 
     
